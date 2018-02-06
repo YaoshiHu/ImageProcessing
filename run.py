8dcfedddd5810138ead12674a993b6c93279b8b8
Yaoshi <32802045+YaoshiHu@users.noreply.github.com>
2018-02-06 01:17:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ import FloydSteinbergDither
 UniformQuan.main('../img/wallpaper.jpg', './unifrom.png', level=8)
 NoiseQuan.main('../img/wallpaper.jpg', './Noise.png', level=4)
 orderDither.main('../img/wallpaper.jpg', './orderDither.png')
-# FloydSteinbergDither.fsDitherQuan('../img/Einstein.jpg', './FStDitherQuan.jpg', 600, 64)
+FloydSteinbergDither.main('../img/wallpaper.jpg', './FStDither.png', 8)
